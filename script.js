c21c7a2fdf4935a431978a99669beb353e54dfc6
ANKITR08 <25bcad08@kristujayanti.com>
2026-03-24 18:35:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("form").addEventListener("submit", function(e) {
-    let phone = document.querySelector("input[name='phone']").value;
-
-    if (phone.length !== 10 || isNaN(phone)) {
-        alert("Enter a valid 10-digit phone number");
-        e.preventDefault();
-    }
-});
